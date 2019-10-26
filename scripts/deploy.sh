17534ae1e45c074dedc8468298bf4d1390867ed9
broskos <broskos@lenovo.com>
2019-10-26 11:04:21 -0400

    Add templates

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+if [ $PWD != $HOME ] ; then echo "USAGE: $0 Must be run from $HOME"; exit 1 ; fi
+
+source ~/dell-lab/scripts/0-site-settings.sh
+
+source ~/stackrc
+cd ~
+time openstack overcloud deploy --templates \
+    --stack $stack_name \
+    -n ~/dell-lab/templates/network_data.yaml \
+    -r ~/dell-lab/templates/roles_data.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
+    -e ~/dell-lab/templates/environments/dell-lab-environment.yaml
+
