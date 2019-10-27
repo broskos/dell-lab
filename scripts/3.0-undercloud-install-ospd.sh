85394a861ef9578a04255cd8891a20e42f773adb
broskos <broskos@lenovo.com>
2019-10-27 08:11:31 -0400

    add in edge nodes to overcloud deploy

--- scripts/3.0-undercloud-install-ospd.sh
+++ scripts/3.0-undercloud-install-ospd.sh
@@ -48,5 +48,7 @@ openstack overcloud image upload --image-path /home/stack/images/ --update-exist
 # Create edge leaf roles #
 ###########################
 ROLES="compute_edge1 compute_edge1vdu compute_edge2 compute_edge2vdu"
-for ROLE in $ROLES; do openstack flavor create --id auto --ram 4096 --disk 40 --vcpus 1 $ROLE ; done
-for ROLE in $ROLES; do openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="$ROLE" --property "resources:CUSTOM_BAREMETAL"="1" --property "resources:DISK_GB"="0" --property "resources:MEMORY_MB"="0" --property "resources:VCPU"="0" $ROLE ; done
+for ROLE in $ROLES;do
+ openstack flavor create --id auto --ram 4096 --disk 40 --vcpus 1 $ROLE
+ openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" --property "capabilities:profile"="$ROLE" --property "resources:CUSTOM_BAREMETAL"="1" --property "resources:DISK_GB"="0" --property "resources:MEMORY_MB"="0" --property "resources:VCPU"="0" $ROLE
+done
