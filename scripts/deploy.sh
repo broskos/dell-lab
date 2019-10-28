f88f836eb1347b8deb519e08370dd6f595fb538c
broskos <broskos@lenovo.com>
2019-10-28 17:14:36 -0400

    add local image list

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,5 +13,6 @@ time openstack overcloud deploy --templates \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
+    -e ~/dell-lab/templates/environments/overcloud-images.yaml \
     -e ~/dell-lab/templates/environments/dell-lab-environment.yaml
 
