db49e17a84a5afa00e56ae78daea410cea3696e1
broskos <broskos@lenovo.com>
2019-10-29 14:56:28 -0400

    add log, comment unused heiradata_override line for now

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,8 +12,10 @@ time openstack overcloud deploy --templates \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-sriov.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
     -e ~/dell-lab/templates/environments/overcloud-images.yaml \
-    -e ~/dell-lab/templates/environments/dell-lab-environment.yaml
+    -e ~/dell-lab/templates/environments/dell-lab-environment.yaml \
+    --log-file deployment.log
+
 
