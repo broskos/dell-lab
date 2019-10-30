5c2843ba428db8c87f917f9fdc084186d91100da
broskos <broskos@lenovo.com>
2019-10-30 07:12:33 -0400

    add rt image parameters to vdu nodes.

--- scripts/3.1-create-upload-rt-image.sh
+++ scripts/3.1-create-upload-rt-image.sh
@@ -4,7 +4,7 @@
 source ~/dell-lab/scripts/0-site-settings.sh
 source /home/stack/stackrc
 
-echo "enter cdn password\n"
+echo "enter cdn password: "
 read CDNPASS
 
 cd ~/images
@@ -14,18 +14,22 @@ virt-customize -a overcloud-realtime-compute.qcow2 --run-command \
 "subscription-manager register --username=$cdn_user --password=$CDNPASS" \
 --run-command "subscription-manager attach --pool $pool"
 
- virt-customize -a overcloud-realtime-compute.qcow2 -v --selinux-relabel \
- --run ~/dell-lab/scripts/rt.sh 2>&1 | tee ~/virt-customize.log
+virt-customize -a overcloud-realtime-compute.qcow2 -v --selinux-relabel \
+--run ~/dell-lab/scripts/rt.sh 2>&1 | tee ~/virt-customize.log
 
 #############################
 # extract vmliuz and initrd #
 #############################
-guestmount -a overcloud-realtime-compute.qcow2 -i --ro image
-cp image/boot/vmlinuz-*.x86_64 ./overcloud-realtime-compute.vmlinuz
-cp image/boot/initramfs-*.x86_64.img ./overcloud-realtime-compute.initrd
-guestunmount image
+rm -Rf /var/tmp/.guestfs*
+mkdir -p ~/images/rt-image
+guestmount -a overcloud-realtime-compute.qcow2 -i --ro rt-image
+cp rt-image/boot/vmlinuz-*.x86_64 ./overcloud-realtime-compute.vmlinuz
+cp rt-image/boot/initramfs-*.x86_64.img ./overcloud-realtime-compute.initrd
+guestunmount rt-image
+rm -Rf ~/images/rt-image
 
 ###########################
 # Upload Images to Glance #
 ###########################
-openstack overcloud image upload --image-path /home/stack/images/ --update-existing
+openstack overcloud image upload --image-path /home/stack/images/ \
+--update-existing --os-image-name overcloud-realtime-compute.qcow2
