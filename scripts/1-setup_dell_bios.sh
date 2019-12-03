add3e205431d7aa95d648fdab9e34f17b584c218
broskos <broskos@lenovo.com>
2019-12-03 05:30:01 -0500

    fixup rt image script fixup setup_dell_bios.sh to configure power mgmt and performance

--- scripts/1-setup_dell_bios.sh
+++ scripts/1-setup_dell_bios.sh
@@ -10,11 +10,12 @@ do
   echo "working on: $NET.$SERVERIP"
 # usefull for clearing out stuck jobqueue:
 # racadm -r $NET.$SERVERIP -u $USER -p $PASSWORD jobqueue delete -i JID_CLEARALL_FORCE
+
   racadm -r $NET.$SERVERIP -u $USER -p $PASSWORD set BIOS.BiosBootSettings.BootMode Bios
 # some systems have the AHCI in a different slot, one of these will error, but whichever is valid should still work
   racadm -r $NET.$SERVERIP -u $USER -p $PASSWORD set BIOS.BiosBootSettings.HddSeq AHCI.Slot.4-1,NonRAID.Slot.6-1
   racadm -r $NET.$SERVERIP -u $USER -p $PASSWORD set BIOS.BiosBootSettings.HddSeq AHCI.Slot.5-1,NonRAID.Slot.6-1
-  racadm -r $NET.$SERVERIP -u $USER -p $PASSWORD set BIOS.IntegratedDevices.SriovGlobalEnable Enabled
+
   racadm -r $NET.$SERVERIP -u $USER -p $PASSWORD set BIOS.IntegratedDevices.SriovGlobalEnable Enabled
   racadm -r $NET.$SERVERIP -u $USER -p $PASSWORD set BIOS.SysProfileSettings.SysProfile Performance
   racadm -r $NET.$SERVERIP -u $USER -p $PASSWORD set BIOS.SysProfileSettings.WorkloadProfile VtOptimizedProfile
