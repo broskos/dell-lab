55f5f0fd5b9f7d695a2fc17effa9a0d71730daf9
broskos <broskos@lenovo.com>
2019-12-11 14:50:53 -0500

    update mac addresses for new virtual controllers

--- scripts/0-site-settings.sh
+++ scripts/0-site-settings.sh
@@ -1,8 +1,14 @@
 
 
+# these are used for RT Image, script needs updated for satellite
+export pool='8a85f9996977b23d0169d99b5860315d'
+export cdn_user='broskos@redhat.com'
+
+# then we use regular satellite
 export satellite=satellite.lab.roskosb.info
 export org=lab
 export director_activation_key='osp13-director-dev'
+
 
 export hostname='director'
 export ip_address='172.17.118.10'
