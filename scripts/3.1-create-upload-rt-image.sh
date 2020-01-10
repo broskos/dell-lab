c57d9cc669cfe715a2df22fb6fd5e92dd0850602
broskos <broskos@lenovo.com>
2020-01-10 09:11:17 -0500

    revert back to cdn for building RT image for now, having trouble with satellite registration

--- scripts/3.1-create-upload-rt-image.sh
+++ scripts/3.1-create-upload-rt-image.sh
@@ -7,15 +7,17 @@ source /home/stack/stackrc
 cd ~/images
 cp -f overcloud-full.qcow2 overcloud-realtime-compute.qcow2
 
-#echo "enter cdn password: "
-#read CDNPASS
-#virt-customize -a overcloud-realtime-compute.qcow2 --run-command \
-#"subscription-manager register --username=$cdn_user --password=$CDNPASS" \
-#--run-command "subscription-manager attach --pool $pool"
+# CDN
+echo "enter cdn password: "
+read CDNPASS
+virt-customize -a overcloud-realtime-compute.qcow2 --run-command \
+"subscription-manager register --username=$cdn_user --password=$CDNPASS" \
+--run-command "subscription-manager attach --pool $pool"
 
-virt-customize -a overcloud-realtime-compute.qcow2 \
---run-command yum localinstall -y "http://$satellite/pub/katello-ca-consumer-latest.noarch.rpm" \
---run-command subscription-manager register --org $org --activationkey $director_activation_key
+# Satellite
+#virt-customize -a overcloud-realtime-compute.qcow2 \
+#--run-command "yum localinstall -y http://$satellite/pub/katello-ca-consumer-latest.noarch.rpm" \
+#--run-command "subscription-manager register --name tempvm.lab.roskosb.info --org $org --activationkey $director_activation_key --force"
 
 virt-customize -a overcloud-realtime-compute.qcow2 -v \
 --copy-in ~/i40e-2.10.19.30-1.x86_64.rpm:/root/ \
