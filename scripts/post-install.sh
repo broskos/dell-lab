c362a9c5f1156bc9645e1af1b942e85b7f289309
broskos <broskos@lenovo.com>
2020-01-27 13:39:43 -0500

    updated for new desired nic configurations (improving numa diversity)

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -14,7 +14,7 @@ openstack flavor create --ram 2048 --disk 20 --vcpus 1 m1.small
 # Create Management Network #
 #############################
 openstack network create --project admin --no-share --provider-network-type vlan --provider-segment 117 --provider-physical-network management management-net
-openstack subnet create --project admin --network management-net --no-dhcp --subnet-range 172.17.117.0/24 --gateway 172.17.117.254 --allocation-pool start=172.17.117.150,end=172.17.117.250 --dns-nameserver 172.17.118.8 management-subnet
+openstack subnet create --project admin --network management-net --no-dhcp --subnet-range 172.17.117.0/24 --gateway 172.17.117.254 --allocation-pool start=172.17.117.5,end=172.17.117.100 --dns-nameserver 172.17.118.8 management-subnet
 
 
 ######################################
