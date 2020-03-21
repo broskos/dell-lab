05c6c1b7fceb1c40e29ea655eba0385096c78928
broskos <broskos@lenovo.com>
2020-03-21 08:48:20 -0400

    updates for RT Image

--- scripts/rt.sh
+++ scripts/rt.sh
@@ -10,7 +10,7 @@ subscription-manager repos \
 --enable=rhel-7-server-extras-rpms \
 --enable=rhel-7-server-openstack-13-rpms
 yum -v -y --setopt=protected_packages= erase kernel.$(uname -m)
-yum -v -y install kernel-rt kernel-rt-kvm kernel-rt-devel tuned-profiles-nfv-host
+yum -v -y install kernel-rt kernel-rt-kvm kernel-rt-devel tuned-profiles-nfv-host kernel-devel gcc
 subscription-manager unregister
 subscription-manager clean
 
