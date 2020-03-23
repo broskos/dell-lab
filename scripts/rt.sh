3a4ed856a134a638a924c0180beeb4db320219e5
broskos <broskos@lenovo.com>
2020-03-23 15:20:30 -0400

    Fixup templates

--- scripts/rt.sh
+++ scripts/rt.sh
@@ -14,6 +14,7 @@ subscription-manager repos \
 --enable=fast-datapath-for-rhel-8-x86_64-rpms
 #dnf -v -y --setopt=protected_packages= erase kernel.$(uname -m)
 dnf -v -y install kernel-rt kernel-rt-kvm kernel-rt-devel tuned-profiles-nfv-host kernel-devel gcc
+grubby --set-default /boot/vmlinux*rt*
 subscription-manager unregister
 subscription-manager clean
 
