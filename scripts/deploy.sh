8cf11b4af073693742f6a6d8fe65131a2c270504
broskos <broskos@lenovo.com>
2020-03-31 11:33:25 -0400

    template updates

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,9 @@ time openstack overcloud deploy --templates \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-sriov.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
     -e ~/dell-lab/templates/containers-prepare-parameter.yaml \
     -e ~/dell-lab/templates/environments/dell-lab-environment.yaml \
     --log-file deployment.log
