21adfdb082bd4d111a097af455f3567c83608b9d
broskos <broskos@lenovo.com>
2020-04-05 18:58:30 -0400

    Spit to multi-stack

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,5 +1,6 @@
 source ~/dell-lab/scripts/0-site-settings.sh
-source ~/vranlabrc
+source ~/central
+stack_name="central"
 
 ####################################
 # fix stackrc for non standard stack name
@@ -25,30 +26,6 @@ openstack flavor create --ram 2048 --disk 20 --vcpus 1 m1.small
 #############################
 openstack network create --project admin --no-share --provider-network-type vlan --provider-segment 117 --provider-physical-network management management-net
 openstack subnet create --project admin --network management-net --no-dhcp --subnet-range 172.17.117.0/24 --gateway 172.17.117.254 --allocation-pool start=172.17.117.2,end=172.17.117.100 --dns-nameserver 172.17.118.8 management-subnet
-
-
-######################################
-# create host aggregates             #
-# to separate edge and central sites #
-######################################
-openstack aggregate create --zone central central
-openstack aggregate create --zone edge1 edge1
-openstack aggregate create --zone edge1vdu edge1vdu
-openstack aggregate create --zone edge2 edge2
-openstack aggregate create --zone edge2vdu edge2vdu
-
-# add computes to central site
-for vm_host in compute-0 compute-1 computehci-0 computehci-1 computehci-2; do
- openstack aggregate add host central "vranlab-$vm_host.lab.local"
-done
-
-# add computes to edge1 site
-openstack aggregate add host edge1 vranlab-compute-e1-0.lab.local
-openstack aggregate add host edge1vdu vranlab-computevdu-e1-0.lab.local
-
-# add computes to edge2 site
-openstack aggregate add host edge2 vranlab-compute-e2-0.lab.local
-openstack aggregate add host edge2vdu vranlab-computevdu-e2-0.lab.local
 
 # increase quotas for admin project
 openstack quota set --cores 200 --instances 100 --ram 500000 --volumes 100 --secgroups 100 --volumes 100 --gigabytes 3072 admin
