5cfc337e29bb19150b65f00c1e91f1eb5d21918c
broskos <broskos@lenovo.com>
2020-05-04 07:34:25 -0400

    fixup undercloud mtu and rt-image script

--- scripts/3.3-create-upload-rt-image.sh
+++ scripts/3.3-create-upload-rt-image.sh
@@ -22,9 +22,9 @@ virt-customize -a overcloud-realtime-compute.qcow2 -v \
 
 # now that the new kernel is installed we can install the igb_uio driver and config tools
 virt-customize -a overcloud-realtime-compute.qcow2 -v \
---run-command "subscription-manager unregister" \
+--run-command "subscription-manager unregister || true" \
 --run-command "subscription-manager clean" \
---copy-in ~/config-fpga-2.0-1.el8.x86_64.rpm:/root/ \
+--copy-in ~/dell-lab/config-fpga/config-fpga-2.0-1.el8.x86_64.rpm:/root/ \
 --run-command "dnf localinstall -y /root/config-fpga-2.0-1.el8.x86_64.rpm" \
 --run-command "chmod +x /etc/rc.d/rc.local" \
 --run-command 'cat << EOF >>  /etc/rc.d/rc.local
