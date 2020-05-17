539d3a798249048d5b83e547c0acec974c2acd2c
broskos <broskos@lenovo.com>
2020-05-17 18:00:11 -0400

    still working on post install for no routed provider nets

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -19,8 +19,8 @@ openstack image create --public --file ~/images/rhel-8.1-x86_64-kvm.qcow2 --disk
 ##########################
 # Create Default Flavors #
 ##########################
-openstack flavor create --ram 2048 --disk 20 --vcpus 1 m1.small
-openstack flavor create --ram 2048 --disk 20 --vcpus 1 --property hw:cpu_policy=dedicated --property hw:mem_page_size=1GB --property hw:emulator_threads_policy=share m1.small-dedicated
+openstack flavor create --ram 2048 --disk 10 --vcpus 1 m1.small
+openstack flavor create --ram 2048 --disk 10 --vcpus 1 --property hw:cpu_policy=dedicated --property hw:mem_page_size=1GB --property hw:emulator_threads_policy=share m1.small-dedicated
 
 #############################
 # Create Management Network as routed provider network#
